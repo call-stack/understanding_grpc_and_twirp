66443d7faef14c410fcd48e832e9af604050697d
kalpitpant <kalpitpant32@gmail.com>
2021-03-25 16:29:47 +0530

    Code for twirp.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-## GRPC
+## Twirp and GRPC
+
+
+- main branch contains the twirp example. 
+    For gRPC example checkout [this branch](https://github.com/kalpitpant/understanding_grpc_and_twirp/tree/gRPC-sample)
--- main.go
+++ main.go
@@ -2,26 +2,16 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net"
+	"net/http"
 
-	"github.com/kalpitpant/understanding-twirp/chat"
-	"github.com/kalpitpant/understanding-twirp/server"
-	"google.golang.org/grpc"
+	"github.com/kalpitpant/understanding-twirp/protoc/fdm"
+	"github.com/kalpitpant/understanding-twirp/server/fdmserver"
 )
 
 func main() {
-	fmt.Println("Go gRPC Beginners Tutorial!")
+	server := &fdmserver.FDMServer{}
+	twirpHandler := fdm.NewFileDownloadManagerServer(server)
 
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 9000))
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-
-	grpcServer := grpc.NewServer()
-	chat.RegisterChatServiceServer(grpcServer, &server.Server{})
-
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %s", err)
-	}
+	fmt.Println("Starting the server....")
+	http.ListenAndServe(":8080", twirpHandler)
 }
